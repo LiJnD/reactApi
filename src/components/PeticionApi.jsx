8f94a538fc37a295ccf98ebb7c0c874d817bc671
LiJnD <luico199@gmal.com>
2022-11-02 12:42:36 -0500

    quinto

--- src/components/PeticionApi.jsx
+++ src/components/PeticionApi.jsx
@@ -1,32 +1,14 @@
 import React from 'react'
-
+import "./Peticion2.css"
 const PeticionApi = () => {
     const [personaje, setPersonaje] = React.useState([])
-    
-
-    const cardStyle = {
-        boxShadow: "0 7px 10px 0 rgba(3, 5, 0, 0.3)",
-        padding: "12px",
-        marginBottom: "10px",
-        textAlign: "center",
-        backgroundColor: '#8c004b',
-        borderRadius: '20px' 
-      };
-      const rootStyle = {
-        margin: "0 auto",
-        width: "max-content",
-        padding: "0 10px",
-        color:"black",
-        fontFamily:"Verdana"
-
-      }
 
     const obtenerPersonaje = async () => {
         try {
-             fetch('https://finalspaceapi.com/api/v0/character')
-            .then ((res) =>  res.json())
-            .then ((data) => setPersonaje(data))
-        }catch (error) {
+            fetch('https://finalspaceapi.com/api/v0/character')
+                .then((res) => res.json())
+                .then((data) => setPersonaje(data))
+        } catch (error) {
             console.log(error)
         }
 
@@ -38,19 +20,24 @@ const PeticionApi = () => {
             <h1>PETICION AL API DE FINAL SPACE</h1>
             <button onClick={obtenerPersonaje}>Traer personaje</button>
             {
+
                 personaje.map((character) => (
-                    <div className='root' style={rootStyle}>
-                    <div className="card" style={cardStyle} key={character.id}>
-                        <div className="card--image">
-                            <img src={character.img_url} alt={character.name} />{" "}
+                    <div className='row'>
+                        <div className='colum'>
+                            <div className='rootStyle'>
+                                <div className="cardStyle" key={character.id}>
+                                    <div className="card--image">
+                                        <img src={character.img_url} alt={character.name} />{" "}
+                                    </div>
+                                    <div className="card--title">{character.name}</div>
+                                </div>
+                            </div>
                         </div>
-                        <div className="card--title">{character.name}</div>
-                    </div>
                     </div>
                 )
                 )
             }
-        </div>
+        </div >
     )
 }
 
